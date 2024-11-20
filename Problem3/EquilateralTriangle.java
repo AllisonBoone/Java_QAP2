38f60d53704c6eec18ed582c3c09fe8619285bcf
Allison <Allison.Boone@keyin.com>
2024-11-20 18:31:43 -0330

    Created EquilateralTriangle class to get the area and perimeter of a Equilateral Triangle

--- Problem3/EquilateralTriangle.java
+++ Problem3/EquilateralTriangle.java
@@ -1,5 +1,18 @@
 package Problem3;
 
-public class EquilateralTriangle {
+// Created EquilateralTriangle class.
+public class EquilateralTriangle extends Triangle{
+
+    // Created the constructor.
+    public EquilateralTriangle(double side) {
+        super(side, side, side);
+        super.name = "Equilateral Triangle";
+    }
+
+    // Created Override.
+    public double getArea() {
+        double side = getPerimeter() / 3;
+        return (Math.sqrt(3) / 4) * side * side;
+    }
     
 }
