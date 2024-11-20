fdac9405acc697003da277dc551d497bf50fdcda
Allison <Allison.Boone@keyin.com>
2024-11-20 17:51:30 -0330

    Created Shape class with name of shape to get area and perimeter

--- Problem3/Shape.java
+++ Problem3/Shape.java
@@ -1,5 +1,21 @@
 package Problem3;
 
-public class Shape {
-    
+// Created Shape class.
+public abstract class Shape {
+    protected String name;
+
+    // Created the constructor.
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    // Created abstract methods.
+    public abstract double getArea();
+    public abstract double getPerimeter();
+
+    // Created toString method.
+    @Override
+    public String toString() {
+        return "Shape: " + name + ", Area: " + getArea() + ", Perimeter: " + getPerimeter();
+    }
 }
