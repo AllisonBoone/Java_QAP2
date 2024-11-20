b7326f5c6c73601c63cbe5dc988b63054e9b61f3
Allison <Allison.Boone@keyin.com>
2024-11-20 17:56:14 -0330

    Created Ellipse class to get the area and perimeter

--- Problem3/Ellipse.java
+++ Problem3/Ellipse.java
@@ -1,5 +1,29 @@
 package Problem3;
 
-public class Ellipse {
+// Created Ellipse class.
+public class Ellipse extends Shape{
+    private double a;
+    private double b;
+
+    // Created the constructor.
+    public Ellipse(double a, double b) {
+        super("Ellipse");
+        if (a < b) {
+            double temp = a;
+            a = b;
+            b = temp;
+        }
+        this.a = a;
+        this.b = b;
+    }
+
+    // Created toString method.
+    public double getArea() {
+        return Math.PI * a * b;
+    }
+
+    public double getPerimeter() {
+        return Math.PI * (3 * (a + b) - Math.sqrt((3 * a + b) *(a + 3 * b)));
+    }
     
 }
