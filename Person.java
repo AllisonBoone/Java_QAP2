7d15176f31006e448512e10c45fe2578a3434aa0
Allison <Allison.Boone@keyin.com>
2024-11-20 16:25:24 -0330

    Created the person class with name, age and gender of the person

---
+++ Person.java
@@ -0,0 +1,29 @@
+// Created person class with name, age and gender of person.
+public class Person {
+    protected String myName;
+    protected int myAge;
+    protected String myGender;
+
+    // Created the constructor.
+    public Person(String name, int age, String gender) {
+        this.myName = name;
+        this.myAge = age;
+        this.myGender = gender;
+    }
+
+    // Created getter and setter methods for name, age and gender.
+    public String getName() { return myName; }
+    public void setName(String name) { this.myName = name; }
+
+    public int getAge() { return myAge; }
+    public void setAge(int age) { this.myAge = age; }
+
+    public String getGender() { return myGender; }
+    public void setGender(String gender) { this.myGender = gender; }
+
+    // Created toString method
+    @Override
+    public String toString() {
+        return myName + ", age: " + myAge + ", gender: " + myGender;
+    }
+}
