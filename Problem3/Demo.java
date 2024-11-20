411fbbc2f217b192a70c81fd65967464c0a6cf56
Allison <Allison.Boone@keyin.com>
2024-11-20 18:35:27 -0330

    Created Demo class and got correct output

--- Problem3/Demo.java
+++ Problem3/Demo.java
@@ -1,5 +1,19 @@
 package Problem3;
 
+// Created Demo class.
 public class Demo {
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[4];
+
+        shapes[0] = new Ellipse(5, 3);
+        shapes[1] = new Circle(4);
+        shapes[2] = new Triangle(3, 4, 5);
+        shapes[3] = new EquilateralTriangle(6);
+ 
+        for (Shape shape : shapes) {
+            System.out.println(shape);
+        }
+
+    }
     
 }
