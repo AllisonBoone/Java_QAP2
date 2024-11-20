03be57bf153c68c443e14302b795148ad60795dc
Allison <Allison.Boone@keyin.com>
2024-11-20 18:27:57 -0330

    Created triangle class to get area and perimeter of a triangle

--- Problem3/Triangle.java
+++ Problem3/Triangle.java
@@ -1,5 +1,31 @@
 package Problem3;
 
-public class Triangle {
+// Created Triangle class.
+public class Triangle extends Shape{
+    private double side1, side2, side3;
+
+    // Created the constructor.
+    public Triangle(double side1, double side2, double side3) {
+        super("Triangle");
+        if (side1 + side2 <= side3 || side2 + side3 <= side1 ||side3 + side1 <= side2) {
+            throw new IllegalArgumentException("Invalid side length for a triangle...");
+        }
     
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+}
+
+// Created Override
+@Override
+public double getArea() {
+    double s = getPerimeter() / 2;
+    return Math.sqrt( s * (s - side1) * (s - side2) * (s - side3));
+}
+
+@Override
+public double getPerimeter() {
+    return side1 + side2 + side3;
+}
+
 }
